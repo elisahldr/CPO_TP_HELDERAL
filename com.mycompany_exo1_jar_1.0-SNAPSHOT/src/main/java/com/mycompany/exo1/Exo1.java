fc51148c2060d280e1e3fabc3ea40427820efa3a
Elisa Helderal <elisa.helderal@epfedu.fr>
2023-09-18 15:20:07 +0200

    fini partie 1

---
+++ com.mycompany_exo1_jar_1.0-SNAPSHOT/src/main/java/com/mycompany/exo1/Exo1.java
@@ -0,0 +1,27 @@
+/* Helderal
+*TP0
+*TDA2
+*apprendre à utiliser java
+*18 septemvre 2023
+*/
+
+package com.mycompany.exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author elisa
+ */
+public class Exo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        System.out.println("Au revoir");
+        String prenom;
+Scanner sc;
+sc = new Scanner(System.in);
+System.out.println("Bonjour, comment est-ce que tu t'appelles?");
+prenom = sc.nextLine();
+    }
+}
