3383156525032bef83f53e03f760c422c3af34ff
Elisa Helderal <elisa.helderal@epfedu.fr>
2023-10-04 11:15:39 +0200

    TD1 stats

---
+++ TP1_stats_HELDERAL/src/TP1_stats_HELDERAL.java
@@ -0,0 +1,25 @@
+
+
+/* Helderal
+*TP1
+*TDA2
+*apprendre à utiliser java
+*26 septempbre 2023
+package tp1_stats_helderal;
+
+
+/**
+ *
+ * @author elisa
+ */
+public class TP1_stats_HELDERAL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
