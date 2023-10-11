be49af8dc153bb6dafee3f87a89f7fa5a3003b2e
Elisa Helderal <elisa.helderal@epfedu.fr>
2023-10-11 10:04:23 +0200

    TD1 stats

--- TP1_stats_HELDERAL/src/TP1_stats_HELDERAL.java
+++ TP1_stats_HELDERAL/src/TP1_stats_HELDERAL.java
@@ -1,4 +1,8 @@
 
+import java.util.Random;
+import java.util.Scanner;
+
+
 
 /* Helderal
 *TP1
@@ -19,68 +23,64 @@ public class TP1_stats_HELDERAL {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        public static void main(String[] args) {
+  
 
-    int face[]=new int[6];
+        int face[] = new int[6];
 
-    Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-    int m;
+        int m;
 
-    System.out.println("Entrez le nombre de lancer que vous voulez faire");
+        System.out.println("Entrez le nombre de lancer que vous voulez faire");
 
-    m=sc.nextInt();
+        m = sc.nextInt();
 
-   
+        for (int i = 0; i < m; i++) {
 
-    for (int i=0; i<m ; i++){
+            Random alea = new Random();
 
-        Random alea = new Random();
+            int lancer = alea.nextInt(6);
 
-        int lancer = alea.nextInt(6);
+            System.out.println(lancer);
 
-        System.out.println(lancer);
+            if (lancer == 0) {
 
-        if (lancer==0){
+                face[0] += 1;
 
-            face[0]+=1;
+            } else if (lancer == 1) {
 
-        } else if(lancer==1){
+                face[1] += 1;
 
-            face[1]+=1;
+            } else if (lancer == 2) {
 
-        } else if(lancer==2){
+                face[2] += 1;
 
-            face[2]+=1;
+            } else if (lancer == 3) {
 
-        } else if(lancer==3){
+                face[3] += 1;
 
-            face[3]+=1;
+            } else if (lancer == 4) {
 
-        } else if(lancer==4){
+                face[4] += 1;
 
-            face[4]+=1;
+            } else if (lancer == 5) {
 
-        } else if(lancer==5){
+                face[5] += 1;
 
-            face[5]+=1;
+            }
+
+        }
+
+        for (int i = 0; i < 6; i++) {
+
+            double pourcent = 0;
+
+            pourcent = face[i] * 100 / m;
+
+            System.out.println("Le pourcentage de face " + (i + 1) + " obtenue est: " + pourcent + "%");
 
         }
 
     }
-
-    for(int i=0; i<6; i++){
-
-        double pourcent = 0;
-
-        pourcent= face[i]*100/m;
-
-        System.out.println("Le pourcentage de face "+ (i+1) +" obtenue est: "+pourcent+"%");
-
-    }
-
-    }
-
-   
 
 }
