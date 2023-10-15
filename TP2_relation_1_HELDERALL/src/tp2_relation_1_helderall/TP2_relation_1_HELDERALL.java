1162c4c2486a15af8b9fb578fa0196eeb7b018c6
Elisa Helderal <elisa.helderal@epfedu.fr>
2023-10-15 23:15:14 +0200

    TP2 relation

---
+++ TP2_relation_1_HELDERALL/src/tp2_relation_1_helderall/TP2_relation_1_HELDERALL.java
@@ -0,0 +1,34 @@
+/* Helderal
+*TP2
+*TDA2
+*apprendre à utiliser java
+*8 octobre 2023
+*/
+package tp2_relation_1_helderall;
+
+/**
+ *
+ * @author elisa
+ */
+public class TP2_relation_1_HELDERALL {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /* Helderal
+/* Helderal
+*TP2
+*TDA2
+*apprendre à utiliser java
+*8 octobre 2023
+
+
+package tp2_manip_helderal;
+ */
+        
+        
+    }
+    
+}
