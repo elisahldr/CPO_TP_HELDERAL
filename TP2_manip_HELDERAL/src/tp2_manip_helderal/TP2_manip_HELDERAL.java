76f9f7695f22511e6cbaed6487c6f93a94270128
Elisa Helderal <elisa.helderal@epfedu.fr>
2023-10-15 22:50:03 +0200

    TP2 manip main

--- TP2_manip_HELDERAL/src/tp2_manip_helderal/TP2_manip_HELDERAL.java
+++ TP2_manip_HELDERAL/src/tp2_manip_helderal/TP2_manip_HELDERAL.java
@@ -4,6 +4,8 @@
  */
 package tp2_manip_helderal;
 
+import java.util.Random;
+
 /**
  *
  * @author elisa
@@ -18,11 +20,21 @@ public class TP2_manip_HELDERAL {
         Tartiflette assiette1 = new Tartiflette(500);
         Tartiflette assiette2 = new Tartiflette(600);
         Tartiflette assiette3 = assiette2;
-        System.out.println("nb de calories de Assiette 2 : "+ assiette2.nbCalories);
-        System.out.println("nb de calories de Assiette 3 : "+ assiette3.nbCalories);
-        Tartiflette assiette2 = assiette1;
-        Tartiflette assiette1 = assiette3;
+        System.out.println("nb de calories de Assiette 2 : " + assiette2.nbCalories);
+        System.out.println("nb de calories de Assiette 3 : " + assiette3.nbCalories);
 
+        Tartiflette remp = assiette1;
+        assiette1 = assiette2;
+        assiette2 = remp;
+
+//Moussaka assiette666 = assiette1 ;
+//Moussaka assiette667 = new Tartiflette() ;  
+//Les deux lignes ne sont pas correctes car les references ne vont pas aux bons objets
+        Random generateurAleat = new Random();
+        Moussaka[] tableauMoussaka = new Moussaka[10];
+        for (int i = 0; i < tableauMoussaka.length; i++) {
+            tableauMoussaka[i] = new Moussaka(generateurAleat.nextInt(1000));
+        }
     }
 
 }
